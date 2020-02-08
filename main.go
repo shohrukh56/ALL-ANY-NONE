34ced340c1fa12eedbda783605650d4f6dae3473
shohrukh <shohrukh5665@gmail.com>
2020-02-08 16:37:58 +0500

    fixed: #1

--- main.go
+++ main.go
@@ -5,42 +5,42 @@ import (
 )
 
 type account struct {
-	id int64
-	name string
+	id      int64
+	name    string
 	balance int64
 }
 
-func main()  {
+func main() {
 }
-func Index (items []account, predicate func(item account)bool) int {
-		for index, item :=range items{
-			if predicate(item){
-				return index
-			}
+func Index(items []account, predicate func(item account) bool) int {
+	for index, item := range items {
+		if predicate(item) {
+			return index
 		}
-		return -1
+	}
+	return -1
 }
 
 func All(items []account, predicate func(item account) bool) bool {
-	return Index(items, func(item account)bool{
+	return Index(items, func(item account) bool {
 		return !predicate(item)
-	})==-1
+	}) == -1
 }
 
 func Any(items []account, predicate func(item account) bool) bool {
-	return Index(items,predicate) !=-1
+	return Index(items, predicate) != -1
 }
 
 func None(items []account, predicate func(item account) bool) bool {
-	return Index(items, func(item account)bool{
+	return Index(items, func(item account) bool {
 		return predicate(item)
-	})==-1
+	}) == -1
 }
 
 func Find(items []account, predicate func(item account) bool) (account, error) {
-	if Index(items, predicate)!=-1{
-		return items[(Index(items, predicate))],nil
+	if result := Index(items, predicate); result != -1 {
+		return items[ result] , nil
 	}
-	return account{},fmt.Errorf("Not found")
-}
 
+	return account{}, fmt.Errorf("Not found")
+}
